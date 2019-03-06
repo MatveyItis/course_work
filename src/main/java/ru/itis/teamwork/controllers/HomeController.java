51118fa83e154a53fd40c3714ab3ee5f3f79e3f2
greenmapc <greenmapc@gmail.com>
2019-03-06 23:01:04 +0300

    upd db models

--- src/main/java/ru/itis/teamwork/controllers/HomeController.java
+++ src/main/java/ru/itis/teamwork/controllers/HomeController.java
@@ -1,13 +1,20 @@
 package ru.itis.teamwork.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.itis.teamwork.controllers.util.ControllerUtils;
 import ru.itis.teamwork.models.User;
 import ru.itis.teamwork.services.UserService;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -19,31 +26,49 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public String registrationPage() {
+    public String registrationPage(@AuthenticationPrincipal User authUser) {
+        if (authUser != null) {
+            return "redirect:/users";
+        }
         return "registration";
     }
 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(@AuthenticationPrincipal User authUser) {
+        if (authUser != null) {
+            return "redirect:/users";
+        }
+        return "login";
+    }
+
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String registerUser(@RequestParam String firstName,
-                               @RequestParam String lastName,
-                               @RequestParam String username,
-                               @RequestParam String password,
+    public String registerUser(@Valid User user,
+                               @RequestParam(value = "password2", required = false) String password2,
+                               BindingResult bindingResult,
                                Model model) {
-        User user = new User();
-        user.setFirstName(firstName);
-        user.setLastName(lastName);
-        user.setUsername(username);
-        user.setPassword(password);
+        boolean isConfirmEmpty = StringUtils.isEmpty(password2);
+        if (isConfirmEmpty) {
+            model.addAttribute("password2Error", "Password confirmation cannot be empty");
+        }
+        if (user.getPassword() != null && !user.getPassword().equals(password2)) {
+            model.addAttribute("passwordError", "Passwords are different");
+        }
+        if (isConfirmEmpty || bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
+            model.mergeAttributes(errorsMap);
+            return "registration";
+        }
         if (!userService.addUser(user)) {
-            model.addAttribute("message", "Registration error. See log for details");
+            model.addAttribute("usernameError", "User exists!");
             return "registration";
         }
         return "redirect:/login";
     }
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public String getUsers(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
+    public String getUsers(@AuthenticationPrincipal User user,
+                           Model model) {
+        model.addAttribute("user", user);
         return "users";
     }
 
